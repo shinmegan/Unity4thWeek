3654a74da6549cecc77afcc1dad910f9985e780e
이유신 <smdeks93@naver.com>
2024-05-11 03:03:29 +0900

    Feat: Add Select Character
    - 캐릭터를 클릭하면, 선택창 표시
    - 둘 중 하나를 선택하면, 선택창 비활성화
    - 선택한 캐릭터 표시
    (PopUpController.cs에서 관리)

---
+++ SpartaTown/Assets/Scripts/PopUpController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour
+{
+    //캐릭터 선택창
+    public GameObject popUpImage;
+    //order상 canvas가 popUpImage위에 위치해서 Entername을 꺼야함.
+    public GameObject playerNameInput;
+    //블루캐릭터
+    public GameObject blueProfile;
+    //핑크캐릭터
+    public GameObject pinkProfile;
+    //선택창 띄우는 버튼
+    public GameObject popUpBtn;
+    //블루캐릭터 선택 버튼
+    public GameObject bluePickBtn;
+    //핑크캐릭터 선택 버튼
+    public GameObject pinkPickBtn;
+
+    //캐릭터 선택창 띄우는 메서드
+    public void CharacterChoicePopUp()
+    {
+        popUpImage.SetActive(true);
+        playerNameInput.SetActive(false);
+        popUpBtn.SetActive(false);
+        bluePickBtn.SetActive(true );
+        pinkPickBtn.SetActive(true );
+    }
+
+    //블루선택시 블루 캐릭터 활성화 메서드
+    public void PickBlue()
+    {
+        popUpImage.SetActive(false);
+        playerNameInput.SetActive(true);
+        popUpBtn.SetActive(true);
+        bluePickBtn.SetActive(false);
+        pinkPickBtn.SetActive(false);
+        blueProfile.SetActive(true);
+        pinkProfile.SetActive(false);
+    }
+
+    //핑크선택시 핑크 캐릭터 활성화 메서드
+    public void PickPink()
+    {
+        popUpImage.SetActive(false);
+        playerNameInput.SetActive(true);
+        popUpBtn.SetActive(true);
+        bluePickBtn.SetActive(false);
+        pinkPickBtn.SetActive(false);
+        blueProfile.SetActive(false);
+        pinkProfile.SetActive(true);
+    }
+}
