dd09ea37a48db76123cdeac2c3a8457b55bcaa39
이유신 <smdeks93@naver.com>
2024-05-10 14:21:12 +0900

    fix: following camera and reverse function
    -캐릭터가 움직이는 방향대로 카메라가 이동하여, 기존의 카메라가 비추던 범위에서 벗어나면 마우스 위치에 의한 캐릭터 시선 변경이 되지 않는 오류 발생 -> 실시간으로 마우스 위치를 추적하는 코드를 추가하여, 실시간 위치를 기반으로 캐릭터의 시선을 변경하게 함.

--- SpartaTown/Assets/Scripts/PlayerInputController.cs
+++ SpartaTown/Assets/Scripts/PlayerInputController.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInputController : TopDownController
 {
     private Camera _camera;
     private void Awake()
     {
-        _camera = Camera.main; //mainCamera라는 태그가 붙어있는 카메라를 가져온다.
+        //mainCamera라는 태그가 붙어있는 카메라를 가져온다.
+        _camera = Camera.main; 
     }
 
     public void OnMove(InputValue value)
     {
-        // Debug.Log("OnMove" + value.ToString());
-        Vector2 moveInput = value.Get<Vector2>().normalized; //크기 1인 벡터로 바로 변경.
-        CallMoveEvent(moveInput); //TopDownController 클래스 메서드 사용
-        //실제 움직이는 처리는 여기서 하는게 아니라 PlayerMovement에서 함.
+        //크기 1인 벡터로 바로 변경.
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        //상위 클래스 메서드 사용(실제 움직이는 처리는 TopDownMovement에서 함)
+        CallMoveEvent(moveInput); 
     }
 
-    public void OnLook(InputValue value)
+    public void OnLook(InputValue value)  
     {
-        // Debug.Log("OnLook" + value.ToString());
+        //마우스 입력을 받음
         Vector2 newAim = value.Get<Vector2>();
-        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim); //카메라를 기준으로 스크린 속 마우스 좌표를 월드좌표로 변경
-        newAim = (worldPos - (Vector2)transform.position).normalized; //transform.position에서 월드좌표로 이동.(T -> W) = W - T
+        //카메라를 기준으로 스크린 속 마우스 좌표를 월드좌표로 변경
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        //캐릭터의 시선 방향 = 마우스 월드좌표 - 캐릭터 위치 좌표 (C -> M) = M - C
+        newAim = (worldPos - (Vector2)transform.position).normalized;
 
-        if (newAim.magnitude >= .9f)
         // Vector 값을 실수로 변환
+        if (newAim.magnitude >= .9f)
         {
             CallLookEvent(newAim);
         }
--- SpartaTown/Assets/Scripts/TopDownMovement.cs
+++ SpartaTown/Assets/Scripts/TopDownMovement.cs
@@ -6,19 +6,18 @@ public class TopDownMovement : MonoBehaviour
     //실제로 이동이 일어날 컴포넌트
     private TopDownController movementController;
     private Rigidbody2D movementRigidbody;
-
-    private Vector2 movementDirection = Vector2.zero; //초기값은 제로.
-    private Vector2 mouseDirection = Vector2.zero;
+    private Vector2 movementDirection = Vector2.zero; 
     private Transform tf;
+    private Camera _camera;
 
-    private void Awake() //(Awake)주로 내 컴포넌트안에서 끝나는 것을 정의
+    private void Awake() //주로 내 컴포넌트안에서 끝나는 것을 정의
     {
         // controller랑 TopdownMovement랑 같은 게임오브젝트 안에 있다라는 가정 하에.
         // 같은 게임오브젝트의 TopDownController, Rigidbody를 가져올 것 
         movementController = GetComponent<TopDownController>();
         movementRigidbody = GetComponent<Rigidbody2D>();
-
-        tf = transform; // transform 할당
+        _camera = Camera.main; 
+        tf = transform; 
     }
 
     private void Start()
@@ -32,7 +31,7 @@ public class TopDownMovement : MonoBehaviour
         movementController.OnLookEvent += Look;
     }
 
-    private void FixedUpdate() //물리업데이트와 관련이 있는 FixedUpdate
+    private void FixedUpdate() 
     {
         // 물리 업데이트에서 움직임 적용(rigidbody의 값을 변경)
         ApplyMovement(movementDirection);
@@ -48,19 +47,20 @@ public class TopDownMovement : MonoBehaviour
     private void ApplyMovement(Vector2 direction)
     {
         direction = direction * 5;
-
         movementRigidbody.velocity = direction;
     }
 
 
+    //캐릭터의 시선을 실제로 변경
     private void Look(Vector2 direction)
-    {
-        if (direction.x < tf.position.x)
+    {   //실시간으로 마우스 위치를 추적
+        Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition); 
+        if (mousePosition.x < tf.position.x)
         {
             // 마우스의 x좌표가 캐릭터의 x좌표보다 작으면 좌측을 바라봄
             tf.localScale = new Vector2(-1,1);
         }
-        else if (direction.x > tf.position.x)
+        else if (mousePosition.x > tf.position.x)
         {
             // 마우스의 x좌표가 캐릭터의 x좌표보다 크면 우측을 바라봄
             tf.localScale = new Vector2(1,1);
